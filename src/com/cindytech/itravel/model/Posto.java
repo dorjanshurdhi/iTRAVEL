4229d27e575fc5c8284eedca11a6f979e62dc232
Dorjan Shurdhi <shurdhidorian@gmail.com>
2024-07-17 13:03:37 +0200

    Update Posto.java

--- src/com/cindytech/itravel/model/Posto.java
+++ src/com/cindytech/itravel/model/Posto.java
@@ -2,7 +2,7 @@ package com.cindytech.itravel.model;
 
 public class Posto {
     
-	private Long id;
+    private Long id;
     private String numeroPosto;
     private Passeggero passeggero;
     private Mezzo mezzo;
